9c6444a29c46d1bf0ae1ed9325efb045ac683066
jalusaga <1203jaub@gmail.com>
2025-06-22 19:11:18 -0600

    setting updated

--- myapi/myapi/settings.py
+++ myapi/myapi/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-$sb20m1s@09$6#qtqbi-n10xj#ilq#kjf*58t88vhyw)m*+&x&"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
